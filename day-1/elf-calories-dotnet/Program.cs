54a4a8e3f6ad14b53bbd2fc772f3f2f7891c3534
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-02 21:47:24 +0100

    chore: added part 2 readme and use files for input

--- day-1/elf-calories-dotnet/Program.cs
+++ day-1/elf-calories-dotnet/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+if (args.Length == 0)
+{
+    WriteLine("Please specify the input file path as an argument!");
+    return;
+}
 
-var input = File.ReadAllLines("../input.txt");
+var input = File.ReadAllLines(args[0]);
 
 var caloriesByElf = ParseCaloriesByElf(input);
 
