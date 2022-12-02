de3411f84b1d08ccb8df9a46c3b29c3f248129d1
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-02 21:40:56 +0100

    feat: solve first puzzle

--- day-1/elf-calories-dotnet/Program.cs
+++ day-1/elf-calories-dotnet/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-foreach (var item in args)
+
+var input = File.ReadAllLines("../input.txt");
+
+var caloriesByElf = ParseCaloriesByElf(input);
+
+WriteLine(caloriesByElf.Max(e => e));
+
+IEnumerable<int> ParseCaloriesByElf(string[] input)
 {
-    WriteLine(item);
+    var caloriesCarriedByElf = 0;
+
+    foreach (var line in input)
+    {
+        if (int.TryParse(line, out var calories))
+        {
+            caloriesCarriedByElf += calories;
+        }
+
+        if (string.IsNullOrEmpty(line))
+        {
+            yield return caloriesCarriedByElf;
+            caloriesCarriedByElf = 0;
+        }
+    }
 }
