7c3bbf3983c924a76ee747fc6a256ce6c953285e
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-02 23:27:51 +0100

    chore: refactorings

--- day-2/rock-paper-scissors-dotnet/Program.cs
+++ day-2/rock-paper-scissors-dotnet/Program.cs
@@ -1,58 +1,60 @@
 ﻿using static Choice;
 
 var input = File.ReadAllLines("../puzzle-input.txt");
-
 var lines = input.Select(ParseGuideline).ToList();
-
 var scores = lines.Select(PlayGame).ToList();
 
-scores.ForEach(scores => WriteLine($"{scores.Item1}:{scores.Item2}"));
-
-WriteLine(scores.Select(x => x.Item2).Sum());
+var totalScore = scores.Select(x => x.P2).Sum();
+WriteLine(totalScore);
 
 Guideline ParseGuideline(string line)
 {
     var splitResult = line.Split(" ");
-    Decision opponent;
+    Choice opponent;
     switch (splitResult[0])
     {
-        case "A": opponent = new Decision(Rock); break;
-        case "B": opponent = new Decision(Paper); break;
-        case "C": opponent = new Decision(Scissors); break;
+        case "A": opponent = Rock; break;
+        case "B": opponent = Paper; break;
+        case "C": opponent = Scissors; break;
         default: throw new ArgumentException($"Could not parse {splitResult[0]}");
     }
 
-    Decision you;
-
+    Choice you;
     switch (splitResult[1])
     {
-        case "X": you = new Decision(Rock); break;
-        case "Y": you = new Decision(Paper); break;
-        case "Z": you = new Decision(Scissors); break;
+        case "X": you = Rock; break;
+        case "Y": you = Paper; break;
+        case "Z": you = Scissors; break;
         default: throw new ArgumentException($"Could not parse {splitResult[1]}");
     }
 
     return new Guideline(opponent, you);
 }
 
-(int, int) PlayGame(Guideline guideline)
+Score PlayGame(Guideline guideline)
 {
-    var opponentScore = 0;
-    var yourScore = 0;
+    int GetPointsForChoice(Choice choice) => choice switch
+    {
+        Rock => 1,
+        Paper => 2,
+        Scissors => 3,
+        _ => 0
+    };
+    var yourScore = GetPointsForChoice(guideline.You);
+    var opponentScore = GetPointsForChoice(guideline.Opponent);
 
-    opponentScore += guideline.Opponent.Points;
-    yourScore += guideline.You.Points;
-
-    var game = new Game(guideline.Opponent.Choice, guideline.You.Choice);
+    var game = new Game(guideline.Opponent, guideline.You);
 
     var result = game switch
     {
         { P1: Rock, P2: Rock } => "draw",
         { P1: Paper, P2: Paper } => "draw",
         { P1: Scissors, P2: Scissors } => "draw",
+
         { P1: Scissors, P2: Paper } => "p1",
         { P1: Paper, P2: Rock } => "p1",
         { P1: Rock, P2: Scissors } => "p1",
+
         { P1: Scissors, P2: Rock } => "p2",
         { P1: Paper, P2: Scissors } => "p2",
         { P1: Rock, P2: Paper } => "p2",
@@ -67,29 +69,10 @@ Guideline ParseGuideline(string line)
         _ => (0, 0)
     };
 
-    return (opponentScore + p1Score, yourScore + p2Score);
+    return new Score(opponentScore + p1Score, yourScore + p2Score);
 }
 
 record Game(Choice P1, Choice P2);
-
-enum Choice
-{
-    Rock, Paper, Scissors
-}
-record Decision(Choice Choice)
-{
-    public int Points
-    {
-        get
-        {
-            switch (this.Choice)
-            {
-                case Rock: return 1;
-                case Paper: return 2;
-                case Scissors: return 3;
-                default: return 0;
-            }
-        }
-    }
-};
-record Guideline(Decision Opponent, Decision You);
+record Score(int P1, int P2);
+enum Choice { Rock, Paper, Scissors }
+record Guideline(Choice Opponent, Choice You);
