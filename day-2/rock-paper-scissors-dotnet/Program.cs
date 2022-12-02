5b9ebdea1dac165353a99c3824b8113783c0437e
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-02 23:41:52 +0100

    feat: first try for part 2 of day 2

--- day-2/rock-paper-scissors-dotnet/Program.cs
+++ day-2/rock-paper-scissors-dotnet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static Choice;
+using static Result;
 
 var input = File.ReadAllLines("../puzzle-input.txt");
 var lines = input.Select(ParseGuideline).ToList();
@@ -19,16 +20,16 @@ Guideline ParseGuideline(string line)
         default: throw new ArgumentException($"Could not parse {splitResult[0]}");
     }
 
-    Choice you;
+    Result desiredResult;
     switch (splitResult[1])
     {
-        case "X": you = Rock; break;
-        case "Y": you = Paper; break;
-        case "Z": you = Scissors; break;
+        case "X": desiredResult = Lose; break;
+        case "Y": desiredResult = Draw; break;
+        case "Z": desiredResult = Win; break;
         default: throw new ArgumentException($"Could not parse {splitResult[1]}");
     }
 
-    return new Guideline(opponent, you);
+    return new Guideline(opponent, desiredResult);
 }
 
 Score PlayGame(Guideline guideline)
@@ -40,39 +41,63 @@ Score PlayGame(Guideline guideline)
         Scissors => 3,
         _ => 0
     };
-    var yourScore = GetPointsForChoice(guideline.You);
-    var opponentScore = GetPointsForChoice(guideline.Opponent);
 
-    var game = new Game(guideline.Opponent, guideline.You);
+    var opponentScore = GetPointsForChoice(guideline.Opponent);
+    var yourChoice = GetChoiceBasedOnGuideline(guideline);
+    var yourScore = GetPointsForChoice(yourChoice);
+
+    var game = new Game(guideline.Opponent, yourChoice);
 
     var result = game switch
     {
-        { P1: Rock, P2: Rock } => "draw",
-        { P1: Paper, P2: Paper } => "draw",
-        { P1: Scissors, P2: Scissors } => "draw",
+        { P1: Rock, P2: Rock } => Draw,
+        { P1: Paper, P2: Paper } => Draw,
+        { P1: Scissors, P2: Scissors } => Draw,
 
-        { P1: Scissors, P2: Paper } => "p1",
-        { P1: Paper, P2: Rock } => "p1",
-        { P1: Rock, P2: Scissors } => "p1",
+        { P1: Scissors, P2: Paper } => Lose,
+        { P1: Paper, P2: Rock } => Lose,
+        { P1: Rock, P2: Scissors } => Lose,
 
-        { P1: Scissors, P2: Rock } => "p2",
-        { P1: Paper, P2: Scissors } => "p2",
-        { P1: Rock, P2: Paper } => "p2",
-        _ => "undefined"
+        { P1: Scissors, P2: Rock } => Win,
+        { P1: Paper, P2: Scissors } => Win,
+        { P1: Rock, P2: Paper } => Win,
+        _ => throw new Exception("unhandled")
     };
 
     var (p1Score, p2Score) = result switch
     {
-        "p1" => (6, 0),
-        "p2" => (0, 6),
-        "draw" => (3, 3),
+        Lose => (6, 0),
+        Win => (0, 6),
+        Draw => (3, 3),
         _ => (0, 0)
     };
 
     return new Score(opponentScore + p1Score, yourScore + p2Score);
 }
 
+
+
+static Choice GetChoiceBasedOnGuideline(Guideline guideline)
+{
+    Choice yourChoice;
+    if (guideline.DesiredResult == Draw) yourChoice = guideline.Opponent;
+    else yourChoice = guideline switch
+    {
+        { Opponent: Rock, DesiredResult: Lose } => Scissors,
+        { Opponent: Rock, DesiredResult: Win } => Paper,
+
+        { Opponent: Paper, DesiredResult: Lose } => Rock,
+        { Opponent: Paper, DesiredResult: Win } => Scissors,
+
+        { Opponent: Scissors, DesiredResult: Lose } => Paper,
+        { Opponent: Scissors, DesiredResult: Win } => Rock,
+        _ => throw new Exception("unhandled case")
+    };
+    return yourChoice;
+}
+
 record Game(Choice P1, Choice P2);
 record Score(int P1, int P2);
+enum Result { Win, Lose, Draw };
 enum Choice { Rock, Paper, Scissors }
-record Guideline(Choice Opponent, Choice You);
+record Guideline(Choice Opponent, Result DesiredResult);
