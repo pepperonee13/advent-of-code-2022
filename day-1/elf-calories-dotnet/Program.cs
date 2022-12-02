b41c3ddae1ceb24961bcffaf07fec98c5bb5b5fc
pesz <peter.szarvas@trustbit.tech>
2022-12-02 21:17:25 +0100

    chore: first version

--- day-1/elf-calories-dotnet/Program.cs
+++ day-1/elf-calories-dotnet/Program.cs
@@ -1,5 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-foreach (var item in args)
+
+//TODO: get from command line or file
+var input = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+
+var caloriesByElf = ParseCaloriesByElf(input);
+
+WriteLine(caloriesByElf.Max(e => e));
+
+
+IEnumerable<int> ParseCaloriesByElf(string input)
 {
-    WriteLine(item);
+    var lines = input.Split(Environment.NewLine);
+
+    var caloriesCarriedByElf = 0;
+
+    foreach (var line in lines)
+    {
+        if (int.TryParse(line, out var calories))
+        {
+            caloriesCarriedByElf += calories;
+        }
+
+        if (string.IsNullOrEmpty(line))
+        {
+            yield return caloriesCarriedByElf;
+            caloriesCarriedByElf = 0;
+        }
+    }
 }
