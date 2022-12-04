ea4bd84687d41e4173818bcfc3717597127771f9
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-04 23:51:04 +0100

    feat: first try

--- day-3/ruecksack-reorganisation-dotnet/Program.cs
+++ day-3/ruecksack-reorganisation-dotnet/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var input = File.ReadAllLines("../test-input.txt");
+
+var priorities = "abcdefghijklmnopqrstuvxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    .Select((c, i) => new { index = i, letter = c })
+                    .ToDictionary(key => key.letter, value => value.index + 1);
+
+var total = 0;
+
+foreach (var line in input)
+{
+    //split in half
+    var half = line.Length / 2;
+    var first = line.Take(half).ToHashSet();
+    var second = line.Skip(half);
+
+    //find common letter
+    var commonLetter = first.Intersect(second).FirstOrDefault();
+
+    //get priorities
+    var priority = priorities[commonLetter];
+
+    //sum
+    total += priority;
+}
+
+WriteLine(total);
