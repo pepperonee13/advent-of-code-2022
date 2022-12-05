30df8af00917f6b628d6abe3c88805773de9c589
pepperonee13 <szarvaspeter13+github@gmail.com>
2022-12-05 20:51:20 +0100

    feat: first try

--- day-4/camp-cleanup-dotnet/Program.cs
+++ day-4/camp-cleanup-dotnet/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("../puzzle-input.txt");
+
+var assignmentsToReconsider = 0;
+
+foreach (var line in input)
+{
+    var assignments = line.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+    var sectionsA = ParseSections(assignments[0]);
+    var sectionsB = ParseSections(assignments[1]);
+
+    if (sectionsA.ToHashSet().IsSubsetOf(sectionsB) || sectionsB.ToHashSet().IsSubsetOf(sectionsA))
+    {
+        assignmentsToReconsider++;
+    }
+}
+
+WriteLine(assignmentsToReconsider);
+
+static IEnumerable<int> ParseSections(string source)
+{
+    var assignment = source.Split("-", StringSplitOptions.RemoveEmptyEntries);
+    var start = int.Parse(assignment[0]);
+    var end = int.Parse(assignment[1]);
+    return Enumerable.Range(start, end - start + 1);
+}
